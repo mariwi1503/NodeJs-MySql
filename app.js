2347a97eaa229493e8d1ad50c9e607c1c2f50cc5
ary <mariwi1503@gmail.com>
2021-07-04 08:14:08 +0800

    handling error

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ const bodyParser = require('body-parser')
 const router = require('./routes/mahasiswa')
 
 app.use(morgan('dev')); // karena kita masih dev
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false})); // pake body- parser
 app.use(bodyParser.json());
 
 app.use('/mahasiswa', router)
+
+// handling error
+app.use((req, res, next) => {
+    const error = new Error('tidak ditemukan');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+
+});
 
 module.exports = app;
